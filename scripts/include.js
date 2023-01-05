9a25e3386178e0cb408db4c166bc0172d08b4f04
Patiban <121844527+Ptbnth@users.noreply.github.com>
2023-01-05 16:39:19 +0900

    Delete include.js

--- scripts/include.js
+++
@@ -1,7 +0,0 @@
-
-  window.addEventListener("DOMContentLoaded",(async()=>{
-  let header=await (await fetch("/header.html")).text();
-    let header2=document.createElement("header");
-    header2.innerHTML=header;
-    document.body.appendChild(header2)
-})())
