8279bf18daa472548dab4547dc8cf51f696987ce
Ptbnth <121844527+Ptbnth@users.noreply.github.com>
2023-01-12 15:22:26 +0900

    Delete include2.js

--- include2.js
+++
@@ -1,7 +0,0 @@
-
-  window.addEventListener("DOMContentLoaded",(async()=>{
-  let headertop=await (await fetch("/headertop.html")).text();
-    let headertop2=document.createElement("headertop");
-    headertop2.innerHTML=headertop;
-    document.body.insertBefore(headertop2,document.body.firstChild);
-})())
