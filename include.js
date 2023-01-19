65fdfe357bfc9b3dc56d16f6078e4bd2b8997d43
Ptbnth <121844527+Ptbnth@users.noreply.github.com>
2023-01-19 19:46:52 +0900

    Update include.js

--- include.js
+++ include.js
@@ -5,3 +5,11 @@
     header2.innerHTML=header;
     document.body.appendChild(header2)
 })())
+
+
+  window.addEventListener("DOMContentLoaded",(async()=>{
+  let headertop=await (await fetch("/headertop.html")).text();
+    let headertop2=document.createElement("headertop");
+    headertop2.innerHTML=headertop;
+    document.body.appendChild(headertop2)
+})())
