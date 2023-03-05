fcc980faffdbbef2c696a295a358205bcb074353
Ptbnth <121844527+Ptbnth@users.noreply.github.com>
2023-03-05 20:14:19 +0900

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+	var code=document.getElementsByClassName("script");
+for(let i=0;i<code.length;i++){
+  var div=document.createElement("div");
+  div.style="display: flex;";
+    div.innerHTML=`
+  <button class="button" onclick="copyScripts(this.parentNode.previousElementSibling);">Copy</button>
+  <main style="margin:auto 0 0 0;padding:7px;font-size:15px;opacity:0;transition: opacity 0.2s;">Copied!</main>
+  `;
+	code[i].after(div);
+};
+});
+function copyScripts(scripts){
+	var element = document.createElement("textarea");
+  fetch(`https://Ptbnth.github.io/scripts/codes/${scripts.id}.js`)
+  .then(response => response.text())
+  .then(data => {
+	navigator.clipboard.writeText(data);
+	scripts.nextElementSibling.children[1].style.opacity="0.7";
+	setTimeout(function(){scripts.nextElementSibling.children[1].style.opacity="0"},3000);
+  });
+};
