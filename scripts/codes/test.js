4390605c611d6db03278f8e75570fb28424fbffd
Ptbnth <121844527+Ptbnth@users.noreply.github.com>
2023-03-08 17:22:09 +0900

    (ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ

--- scripts/codes/test.js
+++ scripts/codes/test.js
@@ -1,4 +1,8 @@
 (()=>{
-  console.log('ぼけ');
-  alert('ぼけ');
+  let a = Math.floor(Math.random() * 2 + 1);
+  if(a == 1){
+    console.log('(ﾟ∀ﾟ)ｱﾋｬ');
+  }else{
+    console.log('(ﾟ∀ﾟ)ｱﾋｬ(ﾟ∀ﾟ)ｱﾋｬ');
+  }
 })();
